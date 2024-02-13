6c44ac28c191f30d27d8e95267e16771b3521a08
yohannesluminari <y.luminari@gmail.com>
2024-02-13 14:36:48 +0100

    result css button over(nho è un'aggiunta giusto per)

--- assets/js/result-script.js
+++ assets/js/result-script.js
@@ -1,17 +1,13 @@
+// RISULTATO ESITO ESAME
+const isTrue = false;
+const resultElement = document.getElementById('result');
 
-// // Assumendo che il booleano sia definito come "isTrue"
-// const isTrue = false; // Puoi cambiare questo valore come desideri
-
-// // Selezioniamo l'elemento con id "result"
-// const resultElement = document.getElementById('result');
-
-// // Se "isTrue" è true, cambiamo il contenuto di resultElement in 'ciao', altrimenti in 'nociao'
-// resultElement.textContent = isTrue ? 'You passed the exam' : 'You did not pass the exam';
-// resultElement.style.display = 'block'; // rendiamo visibile il risultato 
+resultElement.textContent = isTrue ? 'You passed the exam' : 'You did not pass the exam';
+resultElement.style.display = 'block'; // rendiamo visibile il risultato 
 
   
 
-//Right or wrong question
+// RISULTATO ESITO DOMANDE GIUSTE/SBAGLIATE SULLE TOTALI
 const correctQuestions = 23;
 const totQuestion = 50;
 const correct_Questions = document.getElementById('correct-question');
@@ -28,7 +24,7 @@ wrong_Questions.style.display = 'block';
 
 
 
-//Right or wrong question percentage
+// PERCENTUALE DOAMDE GIUSTE E SBAGLIATE
 const correctQuestionsPercentage = 23;
 const correct_QuestionsPercentage = document.getElementById('correct-result-percentage');
 
@@ -42,3 +38,4 @@ wrong_QuestionsPercentage.textContent = `${wrongQuestionsPercentage}%`
 wrong_QuestionsPercentage.style.display = 'block';
 
 
+// GRAFICO CIAMBELLA DI DOAMANDE
