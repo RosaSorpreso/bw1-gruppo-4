6d6157fc4243ecb6fafa0ef844dfad2f89860872
Simone Iengo <iengo.simone@gmail.com>
2024-02-13 15:36:50 +0100

    fixed 80% on js benchmark.

---
+++ assets/js/benchmark-script.js
@@ -0,0 +1,203 @@
+// array con le domande 
+const question = [
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What does CPU stand for?",
+      correct_answer: "Central Processing Unit",
+      incorrect_answers: [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      correct_answer: "Final",
+      incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "The logo for Snapchat is a Bell.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "Pointers were not used in the original C programming language; they were added later on in C++.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the most preferred image format used for logos in the Wikimedia database?",
+      correct_answer: ".svg",
+      incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "In web design, what does CSS stand for?",
+      correct_answer: "Cascading Style Sheet",
+      incorrect_answers: [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the code name for the mobile operating system Android 7.0?",
+      correct_answer: "Nougat",
+      incorrect_answers: [
+        "Ice Cream Sandwich",
+        "Jelly Bean",
+        "Marshmallow",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "On Twitter, what is the character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "Linux was first created as an alternative to Windows XP.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+  ];
+
+// logica quiz app //
+const questionElement = document.getElementsByClassName("question");
+const answerButtons = document.getElementsByClassName("btn");
+const nextButton = document.getElementsByClassName("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0; 
+
+window.onload=startQuiz(); 
+
+
+
+// funzione di avvio del quiz
+function startQuiz(){  
+    correntQuestionIndex = 0 ;
+    score = 0;
+    nextButton.innerHtml= "Next";
+    showQuestion();
+}
+// funzione di inserimento domane dell'array nel DOM //
+function showQuestion(){
+    //resetState();
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    let questionElement = document.querySelector('.question');
+    let answerButtons = document.querySelector('.answer');
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;    
+    let answer = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer];
+    answers = answer.sort(() => Math.random() - 0.5);
+    answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerHTML = answer;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer === currentQuestion.correct_answer){
+            button.dataset.correct = true;}
+        button.addEventListener("click", selectAnswer);})}
+
+//funzione che resetta il DOM prima di inserire le nuove domande con le risposte.
+function resetState(){
+    let questionElement = document.querySelector('.question');
+    let answerButtons = document.querySelector('.answer');
+    nextButton.style.display = "none";
+    while(answerButtosn.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+// funzione che permette la selezione delle risposte, che permette il proseguo delle domande
+let correctAnswers = [];
+let incorrectAnswers = [];
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        correctAnswer.push(selectedBtn);
+        score = correctAnswers.length; 
+        score++; // funzione probabilmente da eliminare. 
+    }else{
+        selectedBtn.classList.add("incorrect");
+        incorrectAnswers.push(selctedBtn);
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.ariaDisabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+
+// funzione che regola il bottone next.
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < question.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < question.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+
+
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${question.length}!`;
+    nextButton.innerHTML = "play Again";
+    nextButton.style.display = "block";
+
+} //con questa funzione e' possibile incorporare la pagina di result
+                      //funzione ancora da scrivere 
