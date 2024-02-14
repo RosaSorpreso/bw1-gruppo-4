a3945f907370760080fe654fd6a4ac0897ce3216
Simone Iengo <iengo.simone@gmail.com>
2024-02-14 15:33:43 +0100

    IL BENCHMARK SI PUO FAREEEEE !

--- assets/js/benchmark-script.js
+++ assets/js/benchmark-script.js
@@ -1,105 +1,23 @@
-// array con le domande // da implementare qui la fetch // 
-const question = [
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "What does CPU stand for?",
-      correct_answer: "Central Processing Unit",
-      incorrect_answers: [
-        "Central Process Unit",
-        "Computer Personal Unit",
-        "Central Processor Unit",
-      ],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
-      correct_answer: "Final",
-      incorrect_answers: ["Static", "Private", "Public"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question: "The logo for Snapchat is a Bell.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question:
-        "Pointers were not used in the original C programming language; they were added later on in C++.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "What is the most preferred image format used for logos in the Wikimedia database?",
-      correct_answer: ".svg",
-      incorrect_answers: [".png", ".jpeg", ".gif"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "In web design, what does CSS stand for?",
-      correct_answer: "Cascading Style Sheet",
-      incorrect_answers: [
-        "Counter Strike: Source",
-        "Corrective Style Sheet",
-        "Computer Style Sheet",
-      ],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "What is the code name for the mobile operating system Android 7.0?",
-      correct_answer: "Nougat",
-      incorrect_answers: [
-        "Ice Cream Sandwich",
-        "Jelly Bean",
-        "Marshmallow",
-      ],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "On Twitter, what is the character limit for a Tweet?",
-      correct_answer: "140",
-      incorrect_answers: ["120", "160", "100"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question: "Linux was first created as an alternative to Windows XP.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question:
-        "Which programming language shares its name with an island in Indonesia?",
-      correct_answer: "Java",
-      incorrect_answers: ["Python", "C", "Jakarta"],
-    },
-  ];
 
+//fetch che aggiunge le domande 
+let question = [];
+window.onload = async() =>{ question = await fetchQuestions(10); // qui per cambiare il numero delle domande.
+  console.log(question);
+startQuiz();
+};
+
+async function fetchQuestions(numQuestions) {
+  try {
+      const response = await fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=18&difficulty=easy`);
+      const data = await response.json();
+      return data.results;
+  } catch (error) {
+      console.error('Errore nel caricamento delle domande:', error);
+      return [];
+  }
+}
 // logica quiz app //
+
 const questionElement = document.querySelector('.question');
 const answerButtons = document.querySelector('.answer');
 const nextButton = document.querySelector('.next-btn');
@@ -107,7 +25,6 @@ let timerId;
 let currentQuestionIndex = 0;
 let score = 0; 
 
-window.onload= startQuiz; 
 
 
 // funzione di avvio del quiz
@@ -120,6 +37,7 @@ function startQuiz(){
 // funzione di inserimento domande dell'array nel DOM //
 function showQuestion(){
     resetState();
+    resetTimer();
     let currentQuestion = question[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;    
@@ -136,9 +54,9 @@ function showQuestion(){
         clearTimeout(timerId);
       timerId = setTimeout(() => {
       handleNextButton();}, 30000);
-      })}
+      })};
 
-//funzione che resetta i button con le precedenti domande e risposte senza ricreare 
+//funzione che resetta i button  delle precedenti domande e risposte  
 function resetState(){
     let questionElement = document.querySelector('.question');
     let answerButtons = document.querySelector('.answer');
@@ -148,34 +66,30 @@ function resetState(){
     }
 }
 
-// funzione che permette la selezione delle risposte, che permette il proseguo delle domande
+// funzione che permette la selezione delle risposte, che permette il proseguo delle domande con timer statico impostato a 30 secondi.
+let lastAnswer;
 function selectAnswer(e){
   const selectedBtn = e.target;
-  const isCorrect = selectedBtn.dataset.correct === "true";
-  if(isCorrect){
-      selectedBtn.classList.add("correct"); 
-      score++;
-  }else{
-      selectedBtn.classList.add("incorrect");
-  }
-  Array.from(answerButtons.children).forEach(button => {
-      if(button.dataset.correct === "true"){
-          button.classList.add("correct");
-      }
-      button.disabled = false;
+  lastAnswer = selectedBtn;
+  Array.from(answerButtons.children).forEach(button =>{
+    button.disabled = false;
   });
   nextButton.style.display = "block";
+}
+function handleNextButton(){
   clearTimeout(timerId);
+  resetTimer();
+  if(lastAnswer && lastAnswer.dataset.correct === "true"){
+    score++;
+  }
+  currentQuestionIndex++;
+  if(currentQuestionIndex < question.length){
+    showQuestion();
+  }else {showScore();
+  }
+  lastAnswer = null;
 }
 
-function handleNextButton(){
-    currentQuestionIndex++;
-    if(currentQuestionIndex < question.length){
-        showQuestion();
-    }else{
-        showScore();
-    }
-}
 nextButton.addEventListener("click",()=>{
     if(currentQuestionIndex < question.length){
         handleNextButton();
@@ -184,15 +98,29 @@ nextButton.addEventListener("click",()=>{
     }
 });
 
+// mostra lo score finale.
 function showScore(){
+    console.log(score);
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${question.length}!`;
     nextButton.innerHTML = "play Again";
     nextButton.style.display = "block";
+}
+// funzione che resetta il timer ad ogni domanda 
 
+function resetTimer() {
+  clearInterval(timerInterval); 
+  timePassed = 0;
+  timeLeft = TIME_LIMIT;
+  document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+  setCircleDasharray();
+  remainingPathColor = COLOR_CODES.info.color;
+  document.getElementById("base-timer-path-remaining").classList.remove(COLOR_CODES.alert.color, COLOR_CODES.warning.color);
+  document.getElementById("base-timer-path-remaining").classList.add(COLOR_CODES.info.color);
+  startTimer();
 }
 
-//funzione che gestisce il timer 
+//funzione che gestisce il timer a livello grafico  
 
 const FULL_DASH_ARRAY = 283;
 const WARNING_THRESHOLD = 10;
@@ -242,7 +170,6 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
-startTimer();
 
 function onTimesUp() {
   clearInterval(timerInterval);
