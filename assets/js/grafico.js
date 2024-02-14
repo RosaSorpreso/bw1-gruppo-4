c1875c69bf6ea2c0c1f2dce168139b46b9893117
yohannesluminari <y.luminari@gmail.com>
2024-02-14 14:41:19 +0100

    grafico-ciambella

--- assets/js/grafico.js
+++ assets/js/grafico.js
@@ -4,14 +4,20 @@ const data = {
         label: "Dati",
         data: [40, 60],// i dati arrivano da una funzione
         backgroundColor: ["#D20094", "#00FFFF"],
-        borderWidth: 0,
+        // borderWidth: 1,
+        cutoutPercentage: 90,
       },
     ],
   };
-
+  const options = {
+    responsive: false, // Set responsive to false to control chart dimensions explicitly
+    width: 190, // Set the width of the chart
+    height: 190, // Set the height of the chart
+  };
   const config = {
     type: "doughnut",
     data: data,
+    options: options, 
   };
 
   const myChart = new Chart(document.getElementById("myChart"), config);
