6c403d9d35e444ed3f2f8addbd95a5ca60331ca0
Ivan-Cecchi <i.cecchi@outlook.com>
2024-02-14 09:50:31 +0100

    Donut-Graph-Test

--- assets/js/result-script.js
+++ assets/js/result-script.js
@@ -1,41 +1,63 @@
 // RISULTATO ESITO ESAME
 const isTrue = false;
-const resultElement = document.getElementById('result');
+const resultElement = document.getElementById("result");
 
-resultElement.textContent = isTrue ? 'You passed the exam' : 'You did not pass the exam';
-resultElement.style.display = 'block'; // rendiamo visibile il risultato 
-
-  
+resultElement.textContent = isTrue
+  ? "You passed the exam"
+  : "You did not pass the exam";
+resultElement.style.display = "block"; // rendiamo visibile il risultato
 
 // RISULTATO ESITO DOMANDE GIUSTE/SBAGLIATE SULLE TOTALI
 const correctQuestions = 23;
 const totQuestion = 50;
-const correct_Questions = document.getElementById('correct-question');
+const correct_Questions = document.getElementById("correct-question");
 
 correct_Questions.textContent = `${correctQuestions}/${totQuestion}`;
-correct_Questions.style.display = 'block';
+correct_Questions.style.display = "block";
 
 const wrongQuestions = 14;
-const wrong_Questions = document.getElementById('wrong-question');
+const wrong_Questions = document.getElementById("wrong-question");
 
 wrong_Questions.textContent = `${wrongQuestions}/${totQuestion}`;
-wrong_Questions.style.display = 'block';
-
-
-
+wrong_Questions.style.display = "block";
 
 // PERCENTUALE DOAMDE GIUSTE E SBAGLIATE
 const correctQuestionsPercentage = 23;
-const correct_QuestionsPercentage = document.getElementById('correct-result-percentage');
+const correct_QuestionsPercentage = document.getElementById(
+  "correct-result-percentage"
+);
 
-correct_QuestionsPercentage.textContent = `${correctQuestionsPercentage}%`
-correct_QuestionsPercentage.style.display = 'block';
+correct_QuestionsPercentage.textContent = `${correctQuestionsPercentage}%`;
+correct_QuestionsPercentage.style.display = "block";
 
 const wrongQuestionsPercentage = 14;
-const wrong_QuestionsPercentage = document.getElementById('wrong-result-percentage');
+const wrong_QuestionsPercentage = document.getElementById(
+  "wrong-result-percentage"
+);
 
-wrong_QuestionsPercentage.textContent = `${wrongQuestionsPercentage}%`
-wrong_QuestionsPercentage.style.display = 'block';
-
+wrong_QuestionsPercentage.textContent = `${wrongQuestionsPercentage}%`;
+wrong_QuestionsPercentage.style.display = "block";
 
 // GRAFICO CIAMBELLA DI DOAMANDE
+
+const config = {
+  type: "doughnut",
+  data: data,
+};
+
+const data = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
