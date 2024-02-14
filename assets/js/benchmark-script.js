bc7d582bbcb14c096f4db228b26ea8522047971e
Simone Iengo <iengo.simone@gmail.com>
2024-02-14 11:52:55 +0100

    unified js of benchmark. let's go and fix it !!

--- assets/js/benchmark-script.js
+++ assets/js/benchmark-script.js
@@ -103,7 +103,7 @@ const question = [
 const questionElement = document.querySelector('.question');
 const answerButtons = document.querySelector('.answer');
 const nextButton = document.querySelector('.next-btn');
-
+let timerId;
 let currentQuestionIndex = 0;
 let score = 0; 
 
@@ -133,9 +133,12 @@ function showQuestion(){
         answerButtons.appendChild(button);
         if(answer === currentQuestion.correct_answer){
             button.dataset.correct = true;}
-        button.addEventListener("click", selectAnswer);})}
+        button.addEventListener("click", selectAnswer);
+      setTimeout(() => {
+      handleNextButton();}, 30000);
+      })}
 
-//funzione che resetta il DOM prima di inserire le nuove domande con le risposte.
+//funzione che resetta i button con le precedenti domande e risposte senza ricreare 
 function resetState(){
     let questionElement = document.querySelector('.question');
     let answerButtons = document.querySelector('.answer');
@@ -159,7 +162,7 @@ function selectAnswer(e){
       if(button.dataset.correct === "true"){
           button.classList.add("correct");
       }
-      button.disabled = true;
+      button.disabled = false;
   });
   nextButton.style.display = "block";
 }
@@ -187,3 +190,120 @@ function showScore(){
     nextButton.style.display = "block";
 
 }
+
+//funzione che gestisce il timer 
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 30;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = ` 
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
