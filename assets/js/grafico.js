420656b72c9d8a8965dc93871977ef3b0369e268
yohannesluminari <y.luminari@gmail.com>
2024-02-14 17:13:38 +0100

    result script

--- assets/js/grafico.js
+++ assets/js/grafico.js
@@ -5,14 +5,13 @@ const data = {
         data: [40, 60],// i dati arrivano da una funzione
         backgroundColor: ["#D20094", "#00FFFF"],
         borderWidth: 0,
-        cutoutPercentage: 90,
+        cutoutPercentage: 50, // Increased hole size
       },
     ],
   };
   const options = {
     responsive: false, // Set responsive to false to control chart dimensions explicitly
-    width: 190, // Set the width of the chart
-    height: 190, // Set the height of the chart
+   
   };
   const config = {
     type: "doughnut",
