f67a5e49d13de4de2efa84590ee4a707437140bf
yohannesluminari <y.luminari@gmail.com>
2024-02-15 12:06:59 +0100

    result js

--- assets/js/result-script.js
+++ assets/js/result-script.js
@@ -1,10 +1,13 @@
+// RISULTATO ESITO ESAME
+let YourPercentage = 20;
+let promosso = 60;
 // GRAFICO CIAMBELLA
 
 let graficoCiambella = document.querySelector('.circle'),
   centro = document.querySelector('.main');
 
 let votoDiPartenza = 0, // parte da 0 mi riempie il 25 percento
-  votoPercentualeFinale = 90,
+  votoPercentualeFinale = 20,
   speed = 5;
 
 let progress = setInterval(() => { // funzione a intervalli specificati da speed
@@ -20,20 +23,24 @@ let progress = setInterval(() => { // funzione a intervalli specificati da speed
 
 
 
-// RISULTATO ESITO ESAME
-let YourPercentage = 20;
-let promosso = 60;
+
 
 
 let resultElement = document.getElementById('result');
 
 if (YourPercentage >= promosso) resultElement.textContent = "You passed the exam";
 else resultElement.textContent = "You did not pass the exam";
-// RISULTATO ESITO h3 CPNGRATULAZOIONI
 
+// RISULTATO ESITO h3 CPNGRATULAZOIONI
 let congrat = document.getElementById('complimenti');
 if (YourPercentage >= promosso) congrat.textContent = "Congratulation!";
 else congrat.textContent = "We are so sorry";
+
+
+// CERTIFICATO
+let certificato = document.getElementById('certificato');
+if (YourPercentage >= promosso) certificato.textContent = " We'll send the certificate in few minutes. Check your email (inclouding promotions/span  floder)";
+else certificato.textContent = "Niente Certificato per te bro";
 
 
 // RISULTATO ESITO DOMANDE GIUSTE/SBAGLIATE SULLE TOTALI
